617778521b2f46b9daee4d1047c66e28d00be956
Jaehong Yoo <rouddy@naver.com>
2024-02-10 21:43:56 +0900

    maven upload

--- algorigobleservice/build.gradle.kts
+++ algorigobleservice/build.gradle.kts
@@ -1,6 +1,100 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+    id("signing")
+}
+
+val versionStr = "1.0.0"
+
+fun String.runCommand(workingDir: File = file("./")): String {
+    val parts = this.split("\\s".toRegex())
+    val proc = ProcessBuilder(*parts.toTypedArray())
+        .directory(workingDir)
+        .redirectOutput(ProcessBuilder.Redirect.PIPE)
+        .redirectError(ProcessBuilder.Redirect.PIPE)
+        .start()
+
+    proc.waitFor(1, TimeUnit.MINUTES)
+    return proc.inputStream.bufferedReader().readText().trim()
+}
+
+fun getCurrentGitBranch(): String {
+    var gitBranch = "Unknown branch"
+    try {
+        val workingDir = File("${project.projectDir}")
+        val result = "git rev-parse --abbrev-ref HEAD".runCommand(workingDir)
+        gitBranch = result.trim()
+    } catch (e: Exception) {
+    }
+    return gitBranch
+}
+
+fun getUserName(): String {
+    return System.getProperty("user.name")
+}
+
+val group = "com.algorigo.rx"
+val archivesBaseName = "bleservice"
+
+val versionName = if (getCurrentGitBranch().contains("master")) {
+    versionStr
+} else {
+    "${versionStr}-${getUserName()}-SNAPSHOT"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = group
+            artifactId = archivesBaseName
+            version = versionName
+            pom {
+                name.set("Algorigo Ble Service")
+                description.set("Algorigo Ble Service Library")
+                url.set("https://github.com/Algorigo/AlgorigoBleServiceLibrary")
+                artifact("$buildDir/outputs/aar/${project.name}-release.aar")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("rouddy")
+                        name.set("Rouddy Yoo")
+                        email.set("rouddy@naver.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:https://github.com/Algorigo/AlgorigoBleServiceLibrary")
+                    developerConnection.set("scm:git:https://github.com/Algorigo/AlgorigoBleServiceLibrary")
+                    url.set("https://github.com/Algorigo/AlgorigoBleServiceLibrary")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            url = uri(if (versionName.endsWith("SNAPSHOT")) {
+                findProperty("NEXUS_SNAPSHOT_REPOSITORY_URL") as String
+            } else {
+                findProperty("NEXUS_REPOSITORY_URL") as String
+            })
+            credentials {
+                username = findProperty("nexusUsername") as String
+                password = findProperty("nexusPassword") as String
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
 
 android {
