64bed744d1bd5135fd1c8b87ef2efe4936878b41
Rouddy <rouddy@naver.com>
2024-03-29 11:16:41 +0900

    gradle compile sdk 수정

--- algorigobleservice/build.gradle.kts
+++ algorigobleservice/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.algorigo.algorigobleservice"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 21
--- common_library/build.gradle.kts
+++ common_library/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.algorigo.common_library"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 24
